9d8e22f12cb87a560dbcf9c6c3185a207842426a
Niranjan-KS <niranjan.ks.pro@gmail.com>
2023-07-24 20:44:29 +0530

    commit

---
+++ create_a_download_button/README.md
@@ -0,0 +1,3 @@
+# create_a_download_button
+
+A new Flutter project.
---
+++ create_a_download_button/android/app/src/main/kotlin/com/example/create_a_download_button/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.create_a_download_button
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
